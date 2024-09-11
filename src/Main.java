063317609685950e9c044dcffa496c1aee2865fc
markup54 <marzenakupka@tlen.pl>
2024-09-11 13:13:21 +0200

    trafione

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -73,5 +70,14 @@ public class Main {
         elementy unikatowe
         elementy nie są indeksowane
          */
+
+        LinkedList<Integer> trafione =new LinkedList<>();
+        //trafiione to elementy które występują w wylosowanych i wpisanych
+        for (Integer wpisana: listaLiczbWpisanych) {
+            if(zbiorLiczbLosowych.contains(wpisana)){
+                trafione.add(wpisana);
+            }
+        }
+        System.out.println("Trafione:"+trafione);
     }
 }
