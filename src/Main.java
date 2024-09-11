cceb83473a386ecda4087f20f10b72615a878bde
markup54 <marzenakupka@tlen.pl>
2024-09-11 11:40:13 +0200

    losowanie tablicy i jej wypisywanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        //tablica
+        //musi mieć z góry określony rozmiar
+        //nie można go później zmienić
+        //elementem tablicy mogą byc typy proste i złożone
+        //tablica 6 elementowa z wartościami losowym z zakresu 1,100
+        int [] tablicaLosowych = new int[6];
+        for (int i = 0; i < tablicaLosowych.length; i++) {
+            tablicaLosowych[i] = (int) (Math.random()*100+1);
+        }
+        //wypisywanie elementów tablicy
+        for (int element:tablicaLosowych) {
+            System.out.print(element+", ");
+        }
     }
 }
